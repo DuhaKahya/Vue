d295f1c3b74e4df2cd751668c94f853466840ceb
DuhaKahya <duhakahyaa@gmail.com>
2024-03-24 14:22:51 +0100

    d

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import Register from '../components/Register.vue';
 const routes = [
   { path: '/', component: Home },
   { path: '/about', component: About },
+  { path: '/contact', component: Contact },
   
   { path: '/articles', component: ArticleList },
   { path: '/createarticle', component: CreateArticle },
